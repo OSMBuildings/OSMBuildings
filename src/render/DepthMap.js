bb17c69859433b127138b9ee6484476951248448
Robert Buchholz <rbuch703@gmail.com>
2015-10-23 09:13:27 +0200

    fix invalid parameter to gl.texParameteri() (gl.CLAMP -> gl.CLAMP_TO_EDGE)

--- src/render/DepthMap.js
+++ src/render/DepthMap.js
@@ -38,13 +38,11 @@ render.DepthMap = {
       /* We will be sampling neighboring pixels of the depth texture to create an ambient
        * occlusion map. With the default texture wrap mode 'gl.REPEAT', sampling the neighbors
        * of edge texels would return texels on the opposite edge of the texture, which is not
-       * what we want. Setting the wrap mode to 'gl.CLAMP' instead returns the texels themselves,
-       * which is far more useful for ambient occlusion maps */
-      //FIXME: seems not to have an effect
-      
+       * what we want. Setting the wrap mode to 'gl.CLAMP_TO_EDGE' instead returns 
+       * the texels themselves, which is far more useful for ambient occlusion maps */
       gl.bindTexture(gl.TEXTURE_2D, this.framebuffer.renderTexture.id);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
       
       /* We will explicitly access (neighbor) texel in depth texture to compute the ambient map.
        * So linear interpolation or mip-mapping of texels neither necessary nor desirable.
