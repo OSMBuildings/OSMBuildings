23dde119b84f7bfed7e61a6729daae1f22261390
Derek Kniffin <derek.kniffin@gmail.com>
2016-04-18 10:42:27 -0400

    Update bower build process

---
+++ build/dist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+set -e # exit with nonzero exit code if anything fails
+
+git config user.name "Travis CI"
+git add --force dist/OSMBuildings*
+git commit -m "Distributing a new build"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
