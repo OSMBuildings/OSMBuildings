0b638a7315fdd415d40f8bf95443393a74034269
Jan Marsch <jama@keks.com>
2020-06-03 01:01:31 +0200

    fixed roofDirection to comply with OSM

--- src/triangulate/roofs/index.js
+++ src/triangulate/roofs/index.js
@@ -61,7 +61,7 @@ var createRoof;
   }
 
   function getRidge (direction, center, polygon) {
-    const rad = (direction / 180 - 0.5) * Math.PI;
+    const rad = ((direction-90) / 180 - 0.5) * Math.PI;
     return getRidgeIntersections(center, [Math.cos(rad), Math.sin(rad)], polygon);
   }
 
