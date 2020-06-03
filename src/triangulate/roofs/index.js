675faaf796b30e832847324e1abd11ea2a65ca53
Jan Marsch <jama@keks.com>
2020-06-04 01:01:09 +0200

    fixed round roofs temporarily

--- src/triangulate/roofs/index.js
+++ src/triangulate/roofs/index.js
@@ -17,20 +17,17 @@ var createRoof;
         return roofWithRidge(triangles, properties, polygon, 0, dim, roofColor, wallColor);
       case 'hipped':
         return roofWithRidge(triangles, properties, polygon, 0, dim, roofColor, wallColor);
-        // return FlatRoof(triangles, properties, polygon, dim, roofColor);
       case 'half-hipped':
         return roofWithRidge(triangles, properties, polygon, 0, dim, roofColor, wallColor);
-        // return FlatRoof(triangles, properties, polygon, dim, roofColor);
       case 'gambrel':
         return roofWithRidge(triangles, properties, polygon, 0, dim, roofColor, wallColor);
       case 'mansard':
         return roofWithRidge(triangles, properties, polygon, 0, dim, roofColor, wallColor);
       case 'round':
-        // roofColor = [1, 0, 1];
         return RoundRoof(triangles, properties, polygon, dim, roofColor, wallColor);
       case 'onion':
         return OnionRoof(triangles, polygon, dim, roofColor);
-      // case 'flat':
+      case 'flat':
       default:
         return FlatRoof(triangles, properties, polygon, dim, roofColor);
     }
@@ -218,49 +215,50 @@ var createRoof;
       return FlatRoof(triangles, properties, polygon, dim, roofColor);
     }
 
-    const ridge = getRidge (properties.roofDirection, dim.center, polygon[0]);
-    if (!ridge) {
-      return FlatRoof(triangles, properties, polygon, dim, roofColor);
-    }
-
-    const distances = getRidgeDistances(polygon[0], ridge.index); // TODO: polygon[0] ???
-    const maxDistance = Math.max(...distances);
-
-    
-
-
-
-    const zPos = dim.roofZ;
-    const yNum = split.NUM_Y_SEGMENTS/2;
-
-    const quarterCircle = Math.PI/2;
-    const circleOffset = -quarterCircle;
-
-    let currYAngle, nextYAngle;
-    let x1, y1;
-    let x2, y2;
-    let size1, size2;
-    let newHeight, newZPos;
-
-    // goes top-down
-    for (let i = 0; i < yNum; i++) {
-      currYAngle = ( i   /yNum)*quarterCircle + circleOffset;
-      nextYAngle = ((i+1)/yNum)*quarterCircle + circleOffset;
-
-      x1 = Math.cos(currYAngle);
-      y1 = Math.sin(currYAngle);
-
-      x2 = Math.cos(nextYAngle);
-      y2 = Math.sin(nextYAngle);
-
-      // size1 = x1*dim.size; // width
-      // size2 = x2*dim.size; // width
-
-      newHeight = (y2-y1)*dim.roofHeight;
-      newZPos = zPos - y2*dim.roofHeight;
-
-      // split.cylinder(buffers, center, radius2, radius1, newHeight, newZPos, color);
-    }
+    return FlatRoof(triangles, properties, polygon, dim, roofColor);
+    // const ridge = getRidge (properties.roofDirection, dim.center, polygon[0]);
+    // if (!ridge) {
+    //   return FlatRoof(triangles, properties, polygon, dim, roofColor);
+    // }
+    //
+    // const distances = getRidgeDistances(polygon[0], ridge.index); // TODO: polygon[0] ???
+    // const maxDistance = Math.max(...distances);
+    //
+    //
+    //
+    //
+    //
+    // const zPos = dim.roofZ;
+    // const yNum = split.NUM_Y_SEGMENTS/2;
+    //
+    // const quarterCircle = Math.PI/2;
+    // const circleOffset = -quarterCircle;
+    //
+    // let currYAngle, nextYAngle;
+    // let x1, y1;
+    // let x2, y2;
+    // let size1, size2;
+    // let newHeight, newZPos;
+    //
+    // // goes top-down
+    // for (let i = 0; i < yNum; i++) {
+    //   currYAngle = ( i   /yNum)*quarterCircle + circleOffset;
+    //   nextYAngle = ((i+1)/yNum)*quarterCircle + circleOffset;
+    //
+    //   x1 = Math.cos(currYAngle);
+    //   y1 = Math.sin(currYAngle);
+    //
+    //   x2 = Math.cos(nextYAngle);
+    //   y2 = Math.sin(nextYAngle);
+    //
+    //   // size1 = x1*dim.size; // width
+    //   // size2 = x2*dim.size; // width
+    //
+    //   newHeight = (y2-y1)*dim.roofHeight;
+    //   newZPos = zPos - y2*dim.roofHeight;
+    //
+    //   // split.cylinder(buffers, center, radius2, radius1, newHeight, newZPos, color);
+    // }
   }
 
 /***
